7206454b1aa81ed63fd67a0be63c9ceaace49cd4
disherdesign <drewdisher@fastmail.com>
2017-07-13 14:43:16 -0500

    PapyrusGM

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+,	path = require('path')
+,	bodyParser = require('body-parser')
+,	cors = require('cors')
+,	passport = require('passport')
+,	mongoose = require('mongoose')
+,	config = require('./config/database')
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected to database '+config.database)
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log('Database error: '+err)
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//body parser middleware
+app.use(bodyParser.json());
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('invalid endpoint')
+});
+
+// Send every un-defined route to index.html 
+app.get('*', (req) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.listen(port, () => {
+	console.log('Server started on port '+port)
+});
